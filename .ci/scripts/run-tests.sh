376d24b4abbbba40d9128f04602440347b4ed564
Victor Martinez <VictorMartinezRubio@gmail.com>
2020-03-23 08:55:09 +0000

    ci(jenkins): one script for the setup and tests

--- .ci/scripts/run-tests.sh
+++ .ci/scripts/run-tests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-GO_VERSION=${1:-1.12.7}
-
 # Install Go
 echo "Installing ${GO_VERSION} with gimme."
 eval "$(curl -sL https://raw.githubusercontent.com/travis-ci/gimme/master/gimme | GIMME_GO_VERSION=${GO_VERSION} bash)"
+
+go test ./...
