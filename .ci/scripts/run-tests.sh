407789694a4a9a1c7d303e913ba4756a7ab880d2
Victor Martinez <VictorMartinezRubio@gmail.com>
2020-03-23 15:16:50 +0000

    ci(jenkins): more verbose log

--- .ci/scripts/run-tests.sh
+++ .ci/scripts/run-tests.sh
@@ -4,10 +4,12 @@
 echo "Installing ${GO_VERSION} with gimme."
 eval "$(curl -sL https://raw.githubusercontent.com/travis-ci/gimme/master/gimme | GIMME_GO_VERSION=${GO_VERSION} bash)"
 
-# To transform the test output to junit and be reported in the CI Jenkins
-go get -v -u github.com/jstemmer/go-junit-report
+echo "Run the tests"
 mkdir -p build
-
 export OUT_FILE='build/test-report.out'
 go test ./... | tee ${OUT_FILE}
-go-junit-report < "${OUT_FILE}" > build/junit-report.xml
+
+# To transform the test output to junit and be reported in the CI Jenkins
+echo "Transform test report to JUnit"
+go get -v -u github.com/jstemmer/go-junit-report
+cat "${OUT_FILE}" | go-junit-report > build/junit-report.xml
