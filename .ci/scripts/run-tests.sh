ebfe0c95cf727b82063a8a67606082e525854584
Victor Martinez <VictorMartinezRubio@gmail.com>
2020-03-23 15:07:33 +0000

    ci(jenkins): store test results

--- .ci/scripts/run-tests.sh
+++ .ci/scripts/run-tests.sh
@@ -4,4 +4,10 @@
 echo "Installing ${GO_VERSION} with gimme."
 eval "$(curl -sL https://raw.githubusercontent.com/travis-ci/gimme/master/gimme | GIMME_GO_VERSION=${GO_VERSION} bash)"
 
-go test ./...
+# To transform the test output to junit and be reported in the CI Jenkins
+go get -v -u github.com/jstemmer/go-junit-report
+mkdir -p build
+
+export OUT_FILE='build/test-report.out'
+go test ./... | tee ${OUT_FILE}
+go-junit-report < "${OUT_FILE}" > build/junit-report.xml
