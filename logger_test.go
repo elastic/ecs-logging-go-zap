e356ff56ca5b216e4451bbceafcbfaf25226aa19
simitt <silvia.mitter@elastic.co>
2020-12-28 21:46:14 +0100

    add validation according to shared spec
    * fix: timestamp format to ISO8601
    * add tests for spec validation
    
    implements #19

--- logger_test.go
+++ logger_test.go
@@ -20,8 +20,12 @@ package ecszap
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
+	"strconv"
 	"testing"
+	"time"
 
+	"go.elastic.co/ecszap/internal/spec"
 	"go.uber.org/zap/zapcore"
 
 	errs "github.com/pkg/errors"
@@ -42,9 +46,57 @@ func (tw *testOutput) Write(p []byte) (int, error) {
 
 func (tw *testOutput) Sync() error { return nil }
 
-func (tw *testOutput) requireContains(t *testing.T, keys []string) {
+func (tw *testOutput) validate(t *testing.T, keys []string) {
 	for _, s := range keys {
 		require.Contains(t, tw.m, s)
+	}
+
+	// Fields `log.origin.file.line` and `log.origin.file.name` are logged as
+	// a map under key log.origin. By using the zap jsonEncoder this cannot
+	// be changed. Remove the nested and add a dotted version of the fields
+	if caller, ok := tw.m[callerKey].(map[string]interface{}); ok {
+		for name, val := range caller {
+			tw.m[fmt.Sprintf("%s.%s", callerKey, name)] = val
+			delete(tw.m, name)
+		}
+	}
+	// skip index checks as we do not have control over that with the formatter
+	// skip Default value checks as they are not yet implemented
+	// skip TopLevelField check as ecszap logger logs in dot notation anyways
+	// skip Sanitization as it is not implemented yet
+	for name, val := range tw.m {
+		field, ok := spec.V1.Fields[name]
+		if !ok { // custom field not defined in spec
+			continue
+		}
+		if field.Required { // all required fields must be present in the log line
+			require.Contains(t, tw.m, name)
+			require.NotNil(t, tw.m[name])
+		}
+		if field.Type != "" { // the defined type must be met
+			var ok bool
+			switch field.Type {
+			case "string":
+				_, ok = val.(string)
+			case "datetime":
+				var s string
+				s, ok = val.(string)
+				if _, err := time.Parse("2006-01-02T15:04:05.000Z0700", s); err == nil {
+					ok = true
+				}
+			case "integer":
+				// json.Unmarshal unmarshals into float64 instead of int
+				if _, floatOK := val.(float64); floatOK {
+					_, err := strconv.ParseInt(fmt.Sprintf("%v", val), 10, 64)
+					if err == nil {
+						ok = true
+					}
+				}
+			default:
+				panic(fmt.Errorf("unhandled type %s from specification for field %s", field.Type, name))
+			}
+			require.True(t, ok, fmt.Sprintf("%s: %v", name, val))
+		}
 	}
 }
 
@@ -52,7 +104,7 @@ func (tw *testOutput) reset() {
 	tw.m = make(map[string]interface{})
 }
 
-func TestECSZapLogger_With(t *testing.T) {
+func TestECSZapLogger(t *testing.T) {
 	out := testOutput{}
 
 	for _, tc := range []struct {
@@ -70,20 +122,18 @@ func TestECSZapLogger_With(t *testing.T) {
 			}()},
 	} {
 		t.Run(tc.name, func(t *testing.T) {
-
 			logger := zap.New(tc.core, zap.AddCaller())
 			defer logger.Sync()
 
 			// strongly typed fields
 			logger.Info("testlog", zap.String("foo", "bar"))
-			out.requireContains(t, []string{"ecs.version", "message",
-				"@timestamp", "log.level", "log.origin", "foo"})
+			out.validate(t, []string{"foo", "log.origin"})
 
 			// log a wrapped error
 			out.reset()
 			err := errors.New("boom")
 			logger.Error("some error", zap.Error(errs.Wrap(err, "crash")))
-			out.requireContains(t, []string{"error"})
+			out.validate(t, []string{"error"})
 
 			// Adding logger wide fields and a logger name
 			out.reset()
@@ -91,7 +141,7 @@ func TestECSZapLogger_With(t *testing.T) {
 			logger = logger.With(zap.Error(errors.New("wrapCore Error")))
 			logger = logger.Named("mylogger")
 			logger.Debug("debug message")
-			out.requireContains(t, []string{"log.logger", "foo", "error"})
+			out.validate(t, []string{"foo", "error"})
 
 			// Use loosely typed logger
 			out.reset()
@@ -100,9 +150,7 @@ func TestECSZapLogger_With(t *testing.T) {
 				"foo", "bar",
 				"count", 17,
 			)
-			out.requireContains(t, []string{"ecs.version", "message",
-				"@timestamp", "log.level", "log.origin", "foo", "count"})
-
+			out.validate(t, []string{"log.origin", "foo", "count"})
 			out.reset()
 
 		})
@@ -115,9 +163,10 @@ func TestECSZapLogger_With(t *testing.T) {
 	logger := zap.New(WrapCore(core), zap.AddCaller())
 	defer logger.Sync()
 	logger.With(zap.Error(errors.New("wrapCore"))).Error("boom")
-	out.requireContains(t, []string{"error", "message"})
+	out.validate(t, []string{"message", "error"})
 	assert.Equal(t, "boom", out.m["message"])
 	outErr, ok := out.m["error"].(map[string]interface{})
+
 	require.True(t, ok, out.m["error"])
 	assert.Equal(t, map[string]interface{}{"message": "wrapCore"}, outErr)
 }
